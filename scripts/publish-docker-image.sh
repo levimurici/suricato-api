722fb7ff10caeeb87a32689d0927ace7504ec6a8
levimurici <levi.murici@gmail.com>
2022-06-08 12:46:00 -0300

    Adicionando pipeline CircleCI

---
+++ scripts/publish-docker-image.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+TAG=$(node -p -e "require('./api/package.json').version")
+GIT_TAG=$(git tag -l $TAG);
+
+IMAGE=$( if [ $DOCKER_IMAGE ]; then echo $DOCKER_IMAGE; else echo "dendebaiano/$CIRCLE_PROJECT_REPONAME"; fi;);
+
+if [ "$TAG" == "$GIT_TAG" ]; then
+  echo "The version $TAG already exists!"
+elif [("$CIRCLE_BRANCH" == "master" )]; then
+  git tag $TAG
+  docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+
+  echo "building image $IMAGE:$TAG" | tee $BUILD_REPORT_PATH
+  docker build -t $IMAGE:$TAG . | tee $BUILD_REPORT_PATH
+
+  echo "pushing image $IMAGE:$TAG" | tee $BUILD_REPORT_PATH
+  docker push $IMAGE:$TAG | tee -a $BUILD_REPORT_PATH
+
+  echo "Image $IMAGE:$TAG published on docker hub!"
+  
+  git push origin $TAG | tee -a $BUILD_REPORT_PATH
+  echo "$IMAGE:$TAG created on github!"
+else
+  echo "The version $TAG is unformated!"
+  echo "Use MAJOR.MINOR.PATCH in master"
+fi
